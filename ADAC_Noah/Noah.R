31e2d7847a734c88bf8d3b7980140c284048367c
unknown <Noah.Balz@campus.Lmu.de>
2024-02-27 12:12:12 +0100

    missing Values standartisierung

--- ADAC_Noah/Noah.R
+++ ADAC_Noah/Noah.R
@@ -1,1 +1,11 @@
 data <- read.csv("adac_test.csv") 
+# subset columns 184-275 
+sdata <- data[,184:275]
+ # missende Werte alle als N.A definieren 
+sdata[sdata == ""] <- NA 
+sdata[sdata == "k.a"] <- NA 
+sdata[sdata == "n.b."] <- NA
+sdata[sdatat == "Keine Angabe"] <- NA
+sdata[sdata == "nicht bekannt"] <- NA
+sdata[sdata == "keine Angaben"] <- NA
+
