9387fe1a54e5c5b0ac460fa1c549a132faa6b2f9
unknown <Noah.Balz@campus.Lmu.de>
2024-03-03 17:15:43 +0100

    10

--- ADAC_Noah/Variables.R
+++ ADAC_Noah/Variables.R
@@ -7,12 +7,27 @@ read.csv("cleandedData.csv")
 #2 Fahrzeuggöße/ Zeitverhältniss :
 #Hubgraumgröße, Breite ,Breite inklusiv Spiegel , Höhe 
  
-#3  Sicherheit (#Airbags , Gurt ) : 
+#3  Sicherheit (#Airbags , Gurt, etc  ) : 
 #Marke ,Klasse 
  
 #4 FahrzeugsGeschwindigkeit : 
 #Marke , Klasse , Verbrauch 
 
-#5 Betriebskosten :
+#5 Kosten( Betrieb ,Fix , WertVerlust, Werkstatt, KFZ ) :
 #Marke , Klasse, Typ 
 
+#6 Co2 Wert:
+#Marke , Klasse ,Verbrauch , Größe/ Gewicht 
+
+#7 CO2 Ausstoß :
+# "" 
+
+#8 Fahrgeräusch(dB) :
+#MArke ,Typ , Verbrauch , Größe/Gewicht 
+
+#9 LadeZeit (elektro): 
+ 
+#10 BAtterieKapazität :
+#Modellstart , Marke 
+
+#11
